1e996b1b866e0c5ca1a301bed7a8ee0ea6e0021b
Guilherme Torres <guiatorres8@gmail.com>
2025-04-07 00:19:56 -0300

    configure post route

--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,39 @@
 import { fastifyCors } from '@fastify/cors';
 import { fastify } from 'fastify';
 import {
+  type ZodTypeProvider,
   serializerCompiler,
   validatorCompiler,
 } from 'fastify-type-provider-zod';
+import { z } from 'zod';
 
 const port = 3333;
 
-const app = fastify();
-app.register(fastifyCors, {
-  origin: true,
-});
+const app = fastify().withTypeProvider<ZodTypeProvider>();
+app.register(fastifyCors);
 
-app.get('/hello', () => {
-  return 'Hello World!';
-});
+app.setValidatorCompiler(validatorCompiler);
+app.setSerializerCompiler(serializerCompiler);
+
+app.post(
+  '/subscriptions',
+  {
+    schema: {
+      body: z.object({
+        name: z.string(),
+        email: z.string().email(),
+      }),
+    },
+  },
+  (req, res) => {
+    const { email, name } = req.body;
+
+    return res.status(201).send({
+      name,
+      email,
+    });
+  }
+);
 
 app.listen({ port }).then(() => {
   console.log(`Server running on port ${port}`);
