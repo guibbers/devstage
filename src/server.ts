49f356d1b6583dbfaf6affdc18dbd60ee7b6f1a6
Guilherme Torres <guiatorres8@gmail.com>
2025-04-09 01:09:17 -0300

    integrate dbs

--- src/server.ts
+++ src/server.ts
@@ -10,6 +10,7 @@ import {
 } from 'fastify-type-provider-zod';
 import { z } from 'zod';
 import { env } from './env';
+import { accessInviteLinkRoute } from './routes/access-invite-link';
 import { subscribeToEventRoute } from './routes/subscribe-to-event-route';
 
 const app = fastify().withTypeProvider<ZodTypeProvider>();
@@ -30,6 +31,7 @@ app.register(fastifySwaggerUi, {
 });
 
 app.register(subscribeToEventRoute);
+app.register(accessInviteLinkRoute);
 
 app.listen({ port: env.PORT }).then(() => {
   console.log(`Server running on port ${env.PORT}`);
