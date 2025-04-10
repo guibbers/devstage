983cba30631c07df6aaee8fb2f8b8897823f5497
Guilherme Torres <guiatorres8@gmail.com>
2025-04-10 00:01:14 -0300

    add access to invite clicks

--- src/server.ts
+++ src/server.ts
@@ -10,7 +10,8 @@ import {
 } from 'fastify-type-provider-zod';
 import { z } from 'zod';
 import { env } from './env';
-import { accessInviteLinkRoute } from './routes/access-invite-link';
+import { accessInviteLinkRoute } from './routes/access-invite-link-route';
+import { getSubscriberInviteClicksRoute } from './routes/get-subscriber-invite-clicks-route';
 import { subscribeToEventRoute } from './routes/subscribe-to-event-route';
 
 const app = fastify().withTypeProvider<ZodTypeProvider>();
@@ -32,6 +33,7 @@ app.register(fastifySwaggerUi, {
 
 app.register(subscribeToEventRoute);
 app.register(accessInviteLinkRoute);
+app.register(getSubscriberInviteClicksRoute);
 
 app.listen({ port: env.PORT }).then(() => {
   console.log(`Server running on port ${env.PORT}`);
